3c1abe9292a5841f31d83fb69977afa86933be09
paul <small.paul666@gmail.com>
2019-08-04 13:25:09 +0100

    Merge remote-tracking branch 'refs/remotes/origin/alex'
    Conflicts:
    	sgan.py

--- sgan.py
+++ sgan.py
@@ -1,123 +1,38 @@
 from __future__ import print_function, division
 
-import os
-import pathlib
-# if __debug__:
-#     print("Debugging, so running on CPU!")
-#     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-import random
-
-import pathlib
-from collections import Counter
-from random import shuffle
-
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-from keras.callbacks import TensorBoard
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, GaussianNoise
+from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
-from keras.preprocessing.image import load_img
 from keras.utils import to_categorical
-from keras_preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.models import load_model
 
-from create_gif import natural_keys
-from utils import get_random_string
+import cv2
+
+import matplotlib.pyplot as plt
+
+import numpy as np
 
 # for rtx 20xx cards
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 
-IMAGE_DIMS = 1 # 3 for rgb
-DATA_PATH = '/home/paul/PycharmProjects/mnist_png/training'
-
-
-def get_latest_epoch(param):
-    keys = [natural_keys(x)[1] for x in param]
-    return int(max(keys) + 1)
-
+IMAGE_SIZE = 112
 
 class SGAN:
-    def __init__(self, _run_id=None):
-        if _run_id is None:
-            self.run_id = get_random_string(8)
-        else:
-            self.run_id = _run_id
-        np.random.seed(1337)
-        random.seed(1337)
-        print(f"self.run_id: {self.run_id}")
-
-        pathlib.Path(f"images/{self.run_id}").mkdir(parents=True, exist_ok=True)
-        log_path = f"logs/{self.run_id}"
-        pathlib.Path(log_path).mkdir(parents=True, exist_ok=True)
-        pathlib.Path(f"models/{self.run_id}/generator").mkdir(parents=True, exist_ok=True)
-        pathlib.Path(f"models/{self.run_id}/discriminator").mkdir(parents=True, exist_ok=True)
-        pathlib.Path(f"models/{self.run_id}/combined").mkdir(parents=True, exist_ok=True)
-
-        self.callback = TensorBoard(log_path)
-        self.epoch_offset = 0
-
-        self.train_gen = ImageDataGenerator(
-            featurewise_center=True,
-            featurewise_std_normalization=True
-        )
-
-        target_size = (28, 28)
+    def __init__(self):
+        self.img_rows = IMAGE_SIZE
+        self.img_cols = IMAGE_SIZE
         self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.num_classes = 10
+        self.latent_dim = 100
 
-        if self.channels == 1:
-            color_mode = 'grayscale'
-        elif self.channels == 3:
-            color_mode = "rgb"
-        else:
-            raise
-
-        self.train_generator = self.train_gen.flow_from_directory(
-            #os.path.join("C:/Users", "xfant", "PycharmProjects", "tinder", "photos"),
-            DATA_PATH,
-            # target_size=(280, 280),
-            target_size=target_size,
-            batch_size=32,
-            # batch_size=2,
-            class_mode='binary',
-            # color_mode='grayscale'
-            color_mode=color_mode
-        )
-
-        print("Loading images...")
-        files = self.train_generator.filepaths
-        shuffle(files)
-        if self.channels == 3:
-            color_mode = "rgb"
-        elif self.channels == 1:
-            color_mode = "grayscale"
-        else:
-            raise
-
-        sample_of_data = [
-            np.array(load_img(img, target_size=target_size, color_mode=color_mode).copy()) for img in files
-        ]
-        sample_of_data = np.array(sample_of_data)
-        sample_of_data = np.expand_dims(sample_of_data, axis=-1)
-        print("Fitting...")
-        self.train_gen.fit(sample_of_data)
-        del sample_of_data
-        print(f"Mean: {self.train_gen.mean}")
-        print(f"std: {self.train_gen.std}")
-
-        self.img_shape = self.train_generator.image_shape
-        self.num_classes = self.train_generator.num_classes
-        self.latent_dim = 300
-        # self.latent_dim = 200 # 300 is likely the limit for this GPU at batch size 2 and img [280,280,3]
-        self.rows, self.columns = 3, 4
-        self.img_save_noise = np.random.normal(0, 1, (self.rows * self.columns, self.latent_dim))
-        print(f"img_shape: {self.img_shape}")
         optimizer = Adam(0.0002, 0.5)
 
         # Build and compile the discriminator
@@ -133,7 +48,7 @@ class SGAN:
         self.generator = self.build_generator()
 
         # The generator takes noise as input and generates imgs
-        noise = Input(shape=(self.latent_dim,))
+        noise = Input(shape=(100,))
         img = self.generator(noise)
 
         # For the combined model we will only train the generator
@@ -146,52 +61,31 @@ class SGAN:
         # Trains generator to fool discriminator
         self.combined = Model(noise, valid)
         self.combined.compile(loss=['binary_crossentropy'], optimizer=optimizer)
-        self.callback.set_model(self.combined)
-        if _run_id:
-            latest_generator_path = self.get_latest_weights_path(_run_id, "generator")
-            latest_discriminator_path = self.get_latest_weights_path(_run_id, "discriminator")
-            self.epoch_offset = get_latest_epoch([latest_generator_path, latest_discriminator_path])
-            try:
-                self.generator.load_weights(latest_generator_path)
-            except Exception as e:
-                print(e)
-            try:
-                self.discriminator.load_weights(latest_discriminator_path)
-            except Exception as e:
-                print(e)
-            try:
-                self.combined.load_weights(self.get_latest_weights_path(_run_id, "combined"))
-            except Exception as e:
-                print(e)
-
-    def load_latest_model(self, model_path, model):
-        saved_models = [os.path.join(model_path, model, d) for d in os.listdir(os.path.join(model_path, model))]
-        latest = max(saved_models, key=os.path.getmtime)
-        return load_model(f"{latest}")
 
     def build_generator(self):
+
         model = Sequential()
-        # model.add(Dense(128 * 70 * 70, activation="relu", input_dim=self.latent_dim))# for 280x280x3 images
-        # model.add(Reshape((70, 70, 128)))
-        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))  # for 28x28x3 images
+
+        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
         model.add(Reshape((7, 7, 128)))
         model.add(BatchNormalization(momentum=0.8))
         model.add(UpSampling2D())
         model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(momentum=0.8))
         model.add(UpSampling2D())
         model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(UpSampling2D())
         model.add(Conv2D(64, kernel_size=3, padding="same"))
         model.add(Activation("relu"))
         model.add(BatchNormalization(momentum=0.8))
-        if self.channels == 1:
-            model.add(Conv2D(self.img_shape[-1], kernel_size=1, padding="same"))  # grey
-        elif self.channels == 3:
-            model.add(Conv2D(self.img_shape[-1], kernel_size=3, padding="same"))  # rgb
-        else:
-            raise
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(1, kernel_size=3, padding="same"))
         model.add(Activation("tanh"))
 
         model.summary()
@@ -202,28 +96,30 @@ class SGAN:
         return Model(noise, img)
 
     def build_discriminator(self):
+
         model = Sequential()
-        model.add(Conv2D(
-            self.train_generator.batch_size, kernel_size=3, strides=2,
-            input_shape=self.img_shape, padding="same"
-        ))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
-        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Conv2D(256, kernel_size=IMAGE_DIMS, strides=1, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.25))
-        model.add(Flatten())
+
+        # model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        # model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(BatchNormalization(momentum=0.8))
+        # model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        # model.add(LeakyReLU(alpha=0.2))
+        # model.add(Dropout(0.25))
+        # model.add(Flatten())
+
+        the_net = tf.keras.applications.mobilenet.MobileNet(include_top=False,
+                                                          weights=None,
+                                                          input_tensor=None,
+                                                          input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1))
 
         model.summary()
 
@@ -231,47 +127,59 @@ class SGAN:
 
         features = model(img)
         valid = Dense(1, activation="sigmoid")(features)
-        label = Dense(self.num_classes + 1, activation="softmax")(features)
+        label = Dense(self.num_classes+1, activation="softmax")(features)
 
         return Model(img, [valid, label])
 
-    def train(self, epochs, sample_interval=50):
-        half_batch = self.train_generator.batch_size // 2
-        counter = Counter(self.train_generator.classes)
-        max_val = float(max(counter.values()))
-        cw1 = {class_id: max_val / num_images for class_id, num_images in counter.items()}
+    def train(self, epochs, batch_size=128, sample_interval=50):
+
+        # Load the dataset
+        (X_train, y_train), (_, _) = mnist.load_data()
+
+        # Rescale -1 to 1
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        X_train = np.array([cv2.resize(x, dsize=(IMAGE_SIZE, IMAGE_SIZE)) for x in X_train])
+        # cv2.resize(X_train[None, :, :, :], dsize=(IMAGE_SIZE, IMAGE_SIZE))
+        X_train = cv2.resize(X_train, dsize=(IMAGE_SIZE, IMAGE_SIZE))
+        X_train = np.expand_dims(X_train, axis=3)
+        y_train = y_train.reshape(-1, 1)
+
+        # Class weights:
+        # To balance the difference in occurences of digit class labels.
+        # 50% of labels that the discriminator trains on are 'fake'.
+        # Weight = 1 / frequency
+        half_batch = batch_size // 2
+        cw1 = {0: 1, 1: 1}
         cw2 = {i: self.num_classes / half_batch for i in range(self.num_classes)}
         cw2[self.num_classes] = 1 / half_batch
 
         # Adversarial ground truths
-        valid = np.ones((self.train_generator.batch_size, 1))
-        fake = np.zeros((self.train_generator.batch_size, 1))
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
 
-        for epoch in range(self.epoch_offset, epochs + self.epoch_offset):
+        for epoch in range(epochs):
 
             # ---------------------
             #  Train Discriminator
             # ---------------------
 
-            x, y = self.train_generator.next()
-            if y.shape[0] != self.train_generator.batch_size:
-                print(f"Got non batch size: {y.shape[0]}")
-                continue
+            # Select a random batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
 
             # Sample noise and generate a batch of new images
-            noise = np.random.normal(0, 1, (self.train_generator.batch_size, self.latent_dim))
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
             gen_imgs = self.generator.predict(noise)
 
             # One-hot encoding of labels
-            labels = to_categorical(y, num_classes=self.num_classes + 1)
-            fake_labels = to_categorical(
-                np.full((self.train_generator.batch_size, 1), self.num_classes), num_classes=self.num_classes + 1
-            )
+            labels = to_categorical(y_train[idx], num_classes=self.num_classes+1)
+            fake_labels = to_categorical(np.full((batch_size, 1), self.num_classes), num_classes=self.num_classes+1)
 
             # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(x, [valid, labels], class_weight=[cw1, cw2])
+            d_loss_real = self.discriminator.train_on_batch(imgs, [valid, labels], class_weight=[cw1, cw2])
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, [fake, fake_labels], class_weight=[cw1, cw2])
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
 
             # ---------------------
             #  Train Generator
@@ -279,70 +187,48 @@ class SGAN:
 
             g_loss = self.combined.train_on_batch(noise, valid, class_weight=[cw1, cw2])
 
-            self.write_log(['g_loss'], [g_loss], epoch)
-            self.write_log(['d_loss', 'ukn_loss_1', 'ukn_loss_2', 'acc', 'op_acc'], d_loss, epoch)
-
             # Plot the progress
-            print(
-                f"{epoch} [D loss: {d_loss[0]}, ukn_1: {d_loss[1]}, ukn_2: {d_loss[2]}, "
-                f"acc: {100 * d_loss[3]}%, "
-                f"op_acc: {100 * d_loss[4]}%] [G loss: {g_loss}]"
-            )
+            print ("%d [D loss: %f, acc: %.2f%%, op_acc: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[3], 100*d_loss[4], g_loss))
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
                 self.sample_images(epoch)
-                if epoch % 5000 == 0:
-                    self.save_model(epoch)
 
     def sample_images(self, epoch):
-        gen_imgs = self.generator.predict(self.img_save_noise)
-        gen_imgs = (gen_imgs - 127.5) / 127.5
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_imgs = self.generator.predict(noise)
 
-        fig, axs = plt.subplots(self.rows, self.columns)
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
         cnt = 0
-        for i in range(self.rows):
-            for j in range(self.columns):
-                if self.img_shape[-1] == 1:
-                    axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray', interpolation='none')
-                else:
-                    axs[i, j].imshow(gen_imgs[cnt, :, :, :], interpolation='none')
-                axs[i, j].axis('off')
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+                axs[i,j].axis('off')
                 cnt += 1
-        plt.savefig(f"images/{self.run_id}/hotnot_{epoch}.png")
-        plt.clf()
+        fig.savefig("images/mnist_%d.png" % epoch)
         plt.close()
 
-    def save_model(self, epoch):
+    def save_model(self):
 
         def save(model, model_name):
-            file_name = f"models/{self.run_id}/{model_name}.h5.tmp"
-            model.save_weights(file_name)
-            os.rename(file_name, file_name.replace(".tmp", ""))
+            model_path = "saved_model/%s.json" % model_name
+            weights_path = "saved_model/%s_weights.hdf5" % model_name
+            options = {"file_arch": model_path,
+                        "file_weight": weights_path}
+            json_string = model.to_json()
+            open(options['file_arch'], 'w').write(json_string)
+            model.save_weights(options['file_weight'])
 
-        save(self.generator, f"generator/sgan_generator_{epoch}")
-        save(self.discriminator, f"discriminator/sgan_discriminator_{epoch}")
-        save(self.combined, f"combined/sgan_adversarial_{epoch}")
-
-    def get_latest_weights_path(self, _run_id, model_name):
-        saved_models = [
-            os.path.join("models", _run_id, model_name, d)
-            for d in os.listdir(os.path.join("models", _run_id, model_name))
-            if not d.endswith(".tmp")
-        ]
-        return max(saved_models, key=os.path.getmtime)
-
-    def write_log(self, names, logs, batch_no):
-        for name, value in zip(names, logs):
-            summary = tf.Summary()
-            summary_value = summary.value.add()
-            summary_value.simple_value = value
-            summary_value.tag = name
-            self.callback.writer.add_summary(summary, batch_no)
-            self.callback.writer.flush()
+        save(self.generator, "mnist_sgan_generator")
+        save(self.discriminator, "mnist_sgan_discriminator")
+        save(self.combined, "mnist_sgan_adversarial")
 
 
 if __name__ == '__main__':
     sgan = SGAN()
-    # sgan = SGAN(_run_id="VZMGUSKD")
-    sgan.train(epochs=20000, sample_interval=10)
+    sgan.train(epochs=20000, batch_size=32, sample_interval=50)
+
